55de01d9a9947e87602b1f3c4352ffd0f02c9223
iatenine <iatenine@gmail.com>
2022-07-17 18:17:31 -0500

    Created functioning /representatives route User can now query for representative info based on state, state + district or no specification to view all Invalid responses result in 400 errors handled gracefully and being logged. Future iterations should make use of a proper logger Closes #3

--- __tests__/api.spec.ts
+++ __tests__/api.spec.ts
@@ -1,6 +1,12 @@
 import { app } from "../src/routes/";
 import frisby from "frisby";
 
+jest.mock("../src/services/representatives", () => {
+  return {
+    getRepresentatives: jest.fn(() => []),
+  };
+});
+
 describe("endpoint smoke tests", () => {
   const HOST = "http://localhost";
   const PORT = 3000;
@@ -17,7 +23,7 @@ describe("endpoint smoke tests", () => {
 
   it("/representatives should exist", async function () {
     const endpoint = `${HOST}:${PORT}/representatives/`;
-    const res = await frisby.get(endpoint);
+    const res = await frisby.post(endpoint);
     expect(res.status).toBe(200);
     expect(res.status).not.toBe(203);
   });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,10 @@
 import express from "express";
 import { router } from "./representatives";
 const app = express();
+import bodyParser from "body-parser";
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/representatives", router);
 
 export { app };
