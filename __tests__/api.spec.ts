fb046f018dcbca614ed87907fb93cf10d500f04e
iatenine <iatenine@gmail.com>
2022-07-21 22:25:57 -0500

    Modify /representatives to use GET Closes #9

--- __tests__/api.spec.ts
+++ __tests__/api.spec.ts
@@ -1,20 +1,23 @@
 import { app } from "../src/routes/";
 import frisby from "frisby";
+import { Server } from "http";
 
 jest.mock("../src/services/representatives", () => {
   return {
-    getRepresentatives: jest.fn(() => []),
+    getRepresentatives: jest.fn((query) => {
+      if (query?.state && query?.state.length !== 2) throw new Error();
+      return [{}];
+    }),
   };
 });
 
 describe("endpoint smoke tests", () => {
   const HOST = "http://localhost";
   const PORT = 3001;
-  let server;
+  let server: Server;
 
   beforeAll(() => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    server = app.listen(PORT, () => {});
+    server = app.listen(PORT);
   });
 
   afterAll(() => {
@@ -23,8 +26,15 @@ describe("endpoint smoke tests", () => {
 
   it("/representatives should exist", async function () {
     const endpoint = `${HOST}:${PORT}/representatives/`;
-    const res = await frisby.post(endpoint);
+    const res = await frisby.get(endpoint);
     expect(res.status).toBe(200);
     expect(res.status).not.toBe(203);
+  });
+
+  it("should return a 400 when state input causes an error", async function () {
+    const endpoint = `${HOST}:${PORT}/representatives/?state=FLD`;
+    const res = await frisby.get(endpoint);
+
+    expect(res.status).toBe(400);
   });
 });
